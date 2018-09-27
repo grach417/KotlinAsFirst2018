e1cd5208acfd7c13e3f8b48a63c36c5d9fd4986e
grach417 <grach417@gmail.com>
2018-09-28 00:42:36 +0500

    Решён второй урок(ветвления).

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,7 +1,10 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task2
 
 import lesson1.task1.sqr
+import java.lang.Math.sqrt
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -17,7 +20,15 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val n1 = number / 1000
+    val n2 = number / 100 % 10
+    val n3 = number / 10 % 10
+    val n4 = number % 10
+    if (n1 + n2 == n3 + n4) {
+        return true
+    } else return false
+}
 
 /**
  * Простая
@@ -26,7 +37,21 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    val d1 = kotlin.math.sqrt((sqr(x1) + sqr(y1)).toDouble())
+    val d2 = kotlin.math.sqrt((sqr(x2) + sqr(y2)).toDouble())
+    if ((x1 == x2) || (y1 == y2) || (d1 == d2)) {
+      return  true
+    } else return false
+
+}
+
+
+
+
+
+
+
 
 
 /**
@@ -37,6 +62,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  */
 fun daysInMonth(month: Int, year: Int): Int = TODO()
 
+
 /**
  * Средняя
  *
@@ -45,8 +71,13 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    val s = sqr(x2 - x1) + sqr(y2 - y1)
+    if( kotlin.math.sqrt(s) + r1 <= r2){
+        return true
+    } else return false
 
+}
 /**
  * Средняя
  *
@@ -56,4 +87,9 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    val p = r * s
+    if (a * b <= p || a * c <= p || b * c <= p){
+        return true
+    } else return false
+    }
